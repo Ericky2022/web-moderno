1f56d0c9eb24f55daf80884c68e371d4cc98706b
Ericky2022 <erivaldo.pereira.santana2016@gmail.com>
2024-04-04 20:19:59 -0300

    desafios area e troca

---
+++ Fundamentos/inicio/desafio_area.js
@@ -0,0 +1,4 @@
+const PI = 3.14;
+let raio = 10;
+let area = PI * raio * raio;
+console.log(area);
---
+++ Fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,11 @@
+let a = 7;
+let b = 94;
+
+let x;
+
+x = b;
+b = a;
+a = x;
+
+console.log(a);
+console.log(b);
