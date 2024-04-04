e1bcc75d824626b0dfe2dc1c0e2e002552659988
Ericky2022 <erivaldo.pereira.santana2016@gmail.com>
2024-04-04 17:28:07 -0300

    segunda aula

---
+++ Fundamentos/inicio/comentario.js
@@ -0,0 +1,12 @@
+// console.log é usado para exibir algo na tela do computador!
+console.log("Passo #1");
+console.log("Passo #2");
+console.log("Passo #3"); // mais um exeemplo de console.log!
+
+/*
+
+    Esse é um
+    comentario de
+    muitas 
+    linhas
+    */
